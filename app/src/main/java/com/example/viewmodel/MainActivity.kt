99470dc2720be6ede3ab138949b92adcb1ec21eb
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-10-27 16:14:09 +0700

    Revisi All

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -95,6 +96,9 @@ fun TampilForm(cobaViewModel: CobaViewModel = viewModel()){
             Image(painterResource(id = R.drawable.baseline_arrow_back_24),
                 contentDescription = null,
                 modifier = Modifier)
+            Spacer(modifier = Modifier.padding(start = 50.dp))
+            Text(text = "Register",
+                )
         }
     }
 
