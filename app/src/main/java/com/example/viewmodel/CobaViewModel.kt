0d99002bf4bf566b2bf30e0f850134a23e0e5abd
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-10-27 14:20:37 +0700

    menambhakan status, email di function insertdata

--- app/src/main/java/com/example/viewmodel/CobaViewModel.kt
+++ app/src/main/java/com/example/viewmodel/CobaViewModel.kt
@@ -16,19 +16,28 @@ class CobaViewModel : ViewModel() {
         private set
     var jenisKL: String by mutableStateOf("")
         private set
+    var statusM: String by mutableStateOf("")
+        private set
     var alamat: String by mutableStateOf("")
+        private set
+    var email: String by mutableStateOf("")
         private set
     private val _uiState = MutableStateFlow(DataForm())
     val uiState : StateFlow<DataForm> = _uiState.asStateFlow()
 
-    fun insertData(nm: String, tlp: String, jk: String, alm: String){
+    fun insertData(nm: String, tlp: String, jk: String, alm: String, st: String, em: String){
         namaUsr = nm;
         noTelp = tlp;
         jenisKL = jk;
         alamat = alm;
+        statusM = st;
+        email = em;
     }
 
     fun setJenisK(pilihJK: String){
         _uiState.update { currentState -> currentState.copy(sex = pilihJK) }
+    }
+    fun Menikah(pilihM: String){
+        _uiState.update { currentState -> currentState.copy(status = pilihM)}
     }
 }
