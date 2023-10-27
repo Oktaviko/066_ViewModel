0a7d883ae0246f697e442d7fb130e7b10111f248
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-10-27 14:31:09 +0700

    Seadanya Revisi

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.viewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -32,8 +34,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -80,7 +85,24 @@ fun TampilForm(cobaViewModel: CobaViewModel = viewModel()){
     val uiState by cobaViewModel.uiState.collectAsState()
     dataForm = uiState;
 
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
 
+    ){
+        Row{
+            Image(painterResource(id = R.drawable.baseline_arrow_back_24),
+                contentDescription = null,
+                modifier = Modifier)
+            Text(text = "Register",
+                textAlign = Alignment.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+                )
+
+        }
+    }
     OutlinedTextField(
         value = textNama,
         singleLine = true,
@@ -265,22 +287,3 @@ fun GreetingPreview() {
         TampilLayout()
     }
 }
-
-//Box(
-//        Modifier
-//            .fillMaxWidth()
-//            .padding(20.dp)
-//    ) {
-//        Row {
-//            Image(
-//                painter = painterResource(id = R.drawable.arrow_back),
-//                contentDescription = null,
-//                modifier = Modifier)
-//            Text(text = "Register",
-//                textAlign = TextAlign.Center,
-//                fontWeight = FontWeight.Bold,
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//            )
-//        }
-//    }
